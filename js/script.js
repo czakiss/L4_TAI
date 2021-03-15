3c3aea03e6176707be659989995ce058eebcc294
Kamil Rękas <zekse00@gmail.com>
2021-03-15 08:13:38 +0100

    add

--- js/script.js
+++ js/script.js
@@ -212,3 +212,20 @@ restart.addEventListener('click', function (event) {
     list.style.display = 'block';
     results.style.display = 'none';
 });
+
+
+let button = document.querySelector('.scroll');
+button.addEventListener('click', goToTop);
+function goToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function showTopButton() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.querySelector(".scroll").style.display = "block";
+    } else {
+        document.querySelector(".scroll").style.display = "none";
+    }
+}
+window.onscroll = function() {showTopButton()};
